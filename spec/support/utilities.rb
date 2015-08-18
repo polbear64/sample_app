225c91f30df09b1b1e2ea5542e24f1776eb8cfb0
yuki <tril56027@gmail.com>
2015-08-19 01:23:57 +0900

    Finish user edit, update, index, and destroy actions

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,8 +1,8 @@
 include ApplicationHelper
 
 def valid_signin(user)
-  fill_in "Email", with: user.Email
-  fill_in "Password", with: user.Password
+  fill_in "Email", with: user.email
+  fill_in "Password", with: user.password
   click_button "Sign in"
 end
 
@@ -11,3 +11,25 @@ Rspec::Matchers.define :have_error_message do |message|
     expect(page).to have_selector('div.alert-error', text: message)
   end
 end
+
+def valid_signup
+  fill_in "Name",         with: "Example User"
+  fill_in "Email",        with: "user@example.com"
+  fill_in "Password",     with: "foobar"
+  fill_in "Confirmation", with: "foobar"
+end
+
+def sign_in(user, options={})
+  if options[:no_capybara]
+    #
+    remember_token = User.new_remember_token
+    cookies[:remember_token] = remember_token
+    user.update_attribute(:remember_token, User.encrypt(remember_token))
+  else
+    visit signin_path
+    fill_in "Email",    with: user.email
+    fill_in "Password", with: user.password
+    click_button "Sign in"
+  end
+end
+
